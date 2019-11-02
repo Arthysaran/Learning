acda56b6b077620be7b07b75ede64a87b86772f3
Arthysaran <57275746+Arthysaran@users.noreply.github.com>
2019-11-02 13:15:06 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,34 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:37:50) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def
+SyntaxError: invalid syntax
+>>> def function1():
+	print("Successful run")
+
+	
+>>> print(Function1())
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    print(Function1())
+NameError: name 'Function1' is not defined
+>>> p = Function1()
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    p = Function1()
+NameError: name 'Function1' is not defined
+>>> def function1():
+	print("Successful run")
+p = function1()
+SyntaxError: invalid syntax
+>>> 
+>>> 
+>>> i = 10
+>>> c = 20
+>>> d = i + c
+>>> print ("Value of d is": d)
+SyntaxError: invalid syntax
+>>> print ("Value of d is": d)
+SyntaxError: invalid syntax
+>>> print("value of d", d)
+value of d 30
+>>> 
